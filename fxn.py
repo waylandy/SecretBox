107460180df0f6599bc3703d4c91414d2d82f158
Wayland Yeung <you@example.com>
2019-05-14 13:29:33 -0400

    added fxn dump

---
+++ fxn.py
@@ -0,0 +1,23 @@
+def draw_ss(ss, x, ypos=20, height=2):
+    for s, p in zip(ss, x):
+        if s=='S': # blue:  sheet
+            pat=patches.Rectangle(width=1,height=height,fc='#0000ff',lw=0,xy=(p-0.5, ypos))
+        if s=='H': # red:   helix
+            pat=patches.Rectangle(width=1,height=height,fc='#ff0000',lw=0,xy=(p-0.5, ypos))
+        if s=='L': # black: loop
+            pat=patches.Rectangle(width=1,height=height/4,fc='#ffffff',lw=0, xy=(p-0.5,ypos+(height*(3/8))))
+        plt.gca().add_patch(pat)
+        del pat
+
+
+def read_xvg(xvg):
+    x,y=[],[]
+    r = open(xvg)
+    for l in r:
+        if l[0] not in '#@':
+            l=l.split()
+            x.append(float(l[0]))
+            y.append(float(l[1]))
+    r.close()
+    return x,y
+
